5f378a7eaeca129af5bcf65b843529afd82d394d
Jung Hoon <52675944+JH627@users.noreply.github.com>
2024-10-25 01:53:30 +0900

    ♻️ refactor: BDD 스타일 사용
    1. Mockito를 한번 감쌈

--- cafeKiosk/src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
+++ cafeKiosk/src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -3,6 +3,7 @@ package sample.cafekiosk.spring.api.service.mail;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
@@ -22,8 +23,7 @@ import static org.mockito.Mockito.*;
 class MailServiceTest {
 
     // MailSendClient mailSendClient = mock(MailSendClient.class);와 같은 효과
-    // @Mock
-    @Spy
+    @Mock
     private MailSendClient mailSendClient;
 
     // MailSendHistoryRepository mailSendHistoryRepository = mock(MailSendHistoryRepository.class);와 같은 효과
@@ -39,15 +39,18 @@ class MailServiceTest {
     @Test
     void sendMail() {
         // given
+        BDDMockito.given(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
+                .willReturn(true);
+
         // mock 객체는 기본적으로 Default Return Value를 반환
         // 스파이는 실제 객체를 기반으로 만들어지기 때문에 stubbing이 되지 않음
 //        when(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
 //                .thenReturn(true);
 
         // 대신 이 문법 사용
-        doReturn(true)
-                .when(mailSendClient)
-                .sendEmail(anyString(), anyString(), anyString(), anyString());
+//        doReturn(true)
+//                .when(mailSendClient)
+//                .sendEmail(anyString(), anyString(), anyString(), anyString());
 
         // when
         boolean result = mailService.sendMail("", "", "", "");
