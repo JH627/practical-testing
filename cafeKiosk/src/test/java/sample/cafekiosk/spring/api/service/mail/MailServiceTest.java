a279babd02b0f1a60b68621c6644abb352509f34
Jung Hoon <52675944+JH627@users.noreply.github.com>
2024-10-25 01:23:43 +0900

    ♻️ refactor: @Mock, @InjectMock을 사용하도록 변경
    1. Mock 어노테이션을 사용할 때는 @ExtendWith(MockitoExtension.class) 필요
    2. InjectMocks는 생성자를 보고 Mock 객체로 선언된 객체를 Inject 해줌

--- cafeKiosk/src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
+++ cafeKiosk/src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -2,6 +2,10 @@ package sample.cafekiosk.spring.api.service.mail;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import sample.cafekiosk.spring.client.mail.MailSendClient;
 import sample.cafekiosk.spring.domain.history.mail.MailSendHistory;
 import sample.cafekiosk.spring.domain.history.mail.MailSendHistoryRepository;
@@ -11,17 +15,28 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
+
+// Mock 어노테이션을 사용할 때 필요
+@ExtendWith(MockitoExtension.class)
 class MailServiceTest {
+
+    // MailSendClient mailSendClient = mock(MailSendClient.class);와 같은 효과
+    @Mock
+    private MailSendClient mailSendClient;
+
+    // MailSendHistoryRepository mailSendHistoryRepository = mock(MailSendHistoryRepository.class);와 같은 효과
+    @Mock
+    private MailSendHistoryRepository mailSendHistoryRepository;
+
+    // 생성자를 보고 Mock 객체로 선언된 객체를 Inject 해줌
+    // MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
+    @InjectMocks
+    private MailService mailService;
 
     @DisplayName("메일 전송 테스트")
     @Test
     void sendMail() {
         // given
-        MailSendClient mailSendClient = mock(MailSendClient.class);
-        MailSendHistoryRepository mailSendHistoryRepository = mock(MailSendHistoryRepository.class);
-
-        MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
-
         // mock 객체는 기본적으로 Default Return Value를 반환
         when(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
                 .thenReturn(true);
